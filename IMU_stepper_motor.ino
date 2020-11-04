ef331c0cb741d96aa04e834e5a169d1f8c6e5a0b
Jacob Romeo <ravingrossrr@gmail.com>
2020-11-04 15:25:53 -0500

    Placed the gyro Code inside motor code. Need to replace values of IF's.

--- IMU_stepper_motor.ino
+++ IMU_stepper_motor.ino
@@ -1,26 +1,39 @@
 #include<Wire.h>
-
-const int MPU_addr=0x68;
-int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
-
+//-------------------------
+const int MPU_addr = 0x68;
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY;
+//-------------------------
 int minVal = 9;
 int maxVal = 351;
-
+//-------------------------
 double x;
 double y;
-
-// X direction
+//-------------------------
+// X direction <>
 const int x_step_pin = 8; // define pin for step
 const int x_dir_pin = 10;  // define pin for direction
-
-// Y direction
+float Gyro_angleX[2]; // define array for gyro angle
+float Total_angleX[2]; // define array for total angle
+float Acceleration_angleX[2]; // define array for acceleration angle
+//-------------------------
+// Y direction ^^
 const int y_step_pin = 4; // define pin for step
 const int y_dir_pin = 6;  // define pin for direction
-
+float Gyro_angleY[2]; // define array for gyro angle
+float Total_angleY[2]; // define array for total angle
+float Acceleration_angleY[2]; // define array for acceleration angle
+//-------------------------
 // Defult directions
 int CC = LOW;
 int CCW = HIGH;
-
+//-------------------------
+float elapsedTime, time, timePrev;
+int i;
+float rad_to_deg = 180 / 3.141592654;
+//-------------------------
+float desired_angle = 0; /*This is the angle in which we whant the
+                          balance to stay steady*/
+//-------------------------
 void setup() {
   // Code for sensing angle
   Wire.begin();
@@ -29,70 +42,103 @@ void setup() {
   Wire.write(0);
   Wire.endTransmission(true);
   Serial.begin(9600);
-
+//-------------------------
   // Stepup all pins to the board
   pinMode(x_step_pin,OUTPUT);
   pinMode(x_dir_pin,OUTPUT);
   pinMode(y_step_pin,OUTPUT);
   pinMode(y_dir_pin,OUTPUT);
-
-}
-
+//-------------------------
+  time = millis(); //Start counting time in milliseconds
+  delay(1000);
+}// end setup void
+//-------------------------
 void loop() {
 
+  timePrev = time;  // the previous time is stored before the actual time read
+  time = millis();  // actual time read
+  elapsedTime = (time - timePrev) / 1000;
+//-------------------------
   // Read the MPU 6050
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr,14,true);
-
-  AcX=Wire.read()<<8|Wire.read();
-  AcY=Wire.read()<<8|Wire.read();
-  AcZ=Wire.read()<<8|Wire.read();
-
+//-------------------------
+  AcX = Wire.read()<<8|Wire.read();
+  AcY = Wire.read()<<8|Wire.read();
+  AcZ = Wire.read()<<8|Wire.read();
+//-------------------------
+  /*---X---*/
+  Acceleration_angleX[1] = atan(-1 * (AcX / 16384.0) / sqrt(pow((AcY / 16384.0), 2) + pow((AcZ / 16384.0), 2))) * rad_to_deg;
+  /*---Y---*/
+  Acceleration_angleY[0] = atan((AcY / 16384.0) / sqrt(pow((AcX / 16384.0), 2) + pow((AcZ / 16384.0), 2))) * rad_to_deg;
+//-------------------------
   int xAng = map(AcX,minVal,maxVal,-90,90);
   int yAng = map(AcY,minVal,maxVal,-90,90);
   int zAng = map(AcZ,minVal,maxVal,-90,90);
-
+//-------------------------
   y = RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
   x = RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
-
-
+//-------------------------
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x43); //Gyro data first adress
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 4, true); //Just 4 registers
+//-------------------------
+  GyX = Wire.read() << 8 | Wire.read(); //Once again we shif and sum
+  GyY = Wire.read() << 8 | Wire.read();
+//-------------------------
+  /*---X---*/
+  Gyro_angleX[0] = GyX / 131.0; // dividing by the value the datasheet gave, 131
+  /*---Y---*/
+  Gyro_angleY[1] = GyY / 131.0;
+//-------------------------
+  // implement for the acceleration due to gravity, .98, that affects the angles
+  /*---X axis angle---*/
+  Total_angleY[0] = 0.98 * (Total_angleY[0] + Gyro_angleY[0] * elapsedTime) + 0.02 * Acceleration_angleY[0];
+  /*---Y axis angle---*/
+  Total_angleX[1] = 0.98 * (Total_angleX[1] + Gyro_angleX[1] * elapsedTime) + 0.02 * Acceleration_angleX[1];
+//-------------------------
+// CHANGE THE VALUES IN THE IF STATEMENTS........
   if (x > 10 && x < 180){
+    Serial.println(Total_angleX[1]);
     Serial.println("move forward in the X // ");
     move(x_step_pin,x_dir_pin,CCW,500,600);
    }
-
+//-------------------------
   if (x < 350 && x > 181){
+    Serial.println(Total_angleX[1]);
     Serial.println("move backward in the X // ");
     move(x_step_pin,x_dir_pin,CC,500,600);
    }
-
+//-------------------------
   if (y > 7 && y < 180){
+    Serial.println(Total_angleY[0]);
     Serial.println("move to the right in the Y ");
     move(y_step_pin,y_dir_pin,CCW,25,2000);
    }
-
+//-------------------------
   if (y < 353 && y > 181){
+    Serial.println(Total_angleY[0]);
     Serial.println("move to the left in the Y ");
     move(y_step_pin,y_dir_pin,CC,25,2000);
    }
 // End Loop
 }
-
-void move(int step_pin, int dir_pin, int direction, int speed, int distance){
+//-------------------------
 // Move the servo function
-
+void move(int step_pin, int dir_pin, int direction, int speed, int distance){
+//-------------------------
   // Set the direction
   digitalWrite(dir_pin, direction);
-
+//-------------------------
   // Run
-  for(int x = 0; x<distance; x++) {
+  for(int x = 0; x < distance; x++) {
    digitalWrite(step_pin,HIGH);
    delayMicroseconds(speed);
    digitalWrite(step_pin,LOW);
    delayMicroseconds(speed);
   }
-
-// End of loop
+// End of FOR loop
 }
