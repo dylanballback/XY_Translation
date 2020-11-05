6cefe09231897725196260f636957a5a13eaaae3
dylanballback <dylanballback19@gmail.com>
2020-11-04 19:10:48 -0500

    update distance

--- IMU_stepper_motor.ino
+++ IMU_stepper_motor.ino
@@ -101,24 +101,24 @@ void loop() {
   Total_angleY[1] = 0.98 * (Total_angleY[1] + Gyro_angleY[1] * elapsedTime) + 0.02 * Acceleration_angleY[1];
 //-------------------------
 // CHANGE THE VALUES IN THE IF STATEMENTS........
-  if (Total_angleX[0] > 1 ){
+  if (x > 1 ){
     Serial.println(Total_angleY[1]);
-    move(x_step_pin,x_dir_pin,CCW,500,100);
+    //move(x_step_pin,x_dir_pin,CCW,500,100);
    }
 //-------------------------
-  if (Total_angleX[0] < 1 ){
+  if (x < 1 ){
     Serial.println(Total_angleY[1]);
-    move(x_step_pin,x_dir_pin,CC,500,100);
+    //move(x_step_pin,x_dir_pin,CC,500,100);
    }
 //-------------------------
-  if (Total_angleY[1] > 1 ){
+  if (y > 1 ){
     Serial.println(Total_angleX[0]);
-    move(y_step_pin,y_dir_pin,CCW,25,300);
+    //move(y_step_pin,y_dir_pin,CCW,25,300);
    }
 //-------------------------
-  if (Total_angleY[1] < 1 ){
+  if (y < 1 ){
     Serial.println(Total_angleX[0]);
-    move(y_step_pin,y_dir_pin,CC,25,300);
+    //move(y_step_pin,y_dir_pin,CC,25,300);
    }
 // End Loop
 }
